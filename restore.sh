7096c6b67e38adeba02bfa7b92a8b9e3220bb304
Benoit Gloria <gloria_b@epita.fr>
2025-08-02 00:49:33 +0200

    update alpine + optional restore

--- restore.sh
+++ restore.sh
@@ -2,9 +2,12 @@
 
 echo "do restore"
 
-restic restore latest --target / --path '/mnt/nextcloud'
-restic restore latest --target / --path '/tmp/nextcloud_pgsql.bak'
+[ "${restore_file:-1}" -eq 1 ] && restic restore latest --target / --path '/mnt/nextcloud'
 
-psql < /tmp/nextcloud_pgsql.bak
+if [ "${restore_db:-1}" -eq 1 ]; then
+  echo "restore db"
+  restic restore latest --target / --path '/tmp/nextcloud_pgsql.bak'
+  psql < /tmp/nextcloud_pgsql.bak
+fi
 
 echo "restore done"
